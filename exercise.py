71b0c195f1b611db7cdf4ac3745623437449c9b5
meglioLeo <leonardo.cresti@students.sarrocchi.it>
2024-10-06 15:49:49 +0200

    added the static method + comments

--- exercise.py
+++ exercise.py
@@ -1,17 +1,26 @@
 class Student():
+    
+    student_count = 0  #variable that keeps track of the number of students
+    
     def __init__(self, name, age, serial_number):
         self.name = name
         self.age = age
         self.serial_number = serial_number
+        Student.student_count += 1  #increment the student_count variable by 1
     
     def get_student_info(self):
         print(f"Name: {self.name}, age: {self.age}, serial number: {self.serial_number}")
+        
+    @staticmethod
+    def get_student_count():
+        print(f"Total number of students: {Student.student_count}")
 
-student1 = Student("John", 21, 12345)
-student2 = Student("Alice", 22, 12346)
-student3 = Student("Bob", 23, 12347)
+student1 = Student("Leonardo", 18, 123)
+student2 = Student("Matteo", 17, 456)
+student3 = Student("Davide", 19, 789)
 
 student1.get_student_info()
 student2.get_student_info()
 student3.get_student_info()
 
+Student.get_student_count()  #call the static method
