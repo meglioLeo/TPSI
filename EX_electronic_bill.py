c3eb78f36bb105921f1d10b5baeb9df8eb2ffae3
meglioLeo <leonardo.cresti@students.sarrocchi.it>
2024-10-06 19:38:18 +0200

    need to modify the classmethod

--- EX_electronic_bill.py
+++ EX_electronic_bill.py
@@ -1,3 +1,5 @@
+from datetime import datetime   #import to get the function to obtain the current date
+
 class Bill:
     def __init__(self, client, amount, date):
         self.client = client
@@ -7,3 +9,10 @@ class Bill:
     def __str__(self):
         return f"Client: {self.client}, Amount: {self.amount}, Date: {self.date}"
     
+    @classmethod #Todo
+    def create_daily_bill(cls, client, amount):
+        today = datetime.date.now()
+        return cls(client, amount, today)
+    
+bill1 = Bill.create_daily_bill("John", 100)  #create a bill for John with an amount of 100
+print(bill1)  #print the bill
