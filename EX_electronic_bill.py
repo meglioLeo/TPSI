14df98929fc034ff7d6efabf2a3e630b344b52f6
meglioLeo <leonardo.cresti@students.sarrocchi.it>
2024-10-09 17:36:49 +0200

    added comments

--- EX_electronic_bill.py
+++ EX_electronic_bill.py
@@ -4,15 +4,15 @@ class Bill:
     def __init__(self, client, amount, date):
         self.client = client
         self.amount = amount
-        self.date = date
+        self.date = date           #format of the date is Y-M-D
         
-    def __str__(self):
+    def __str__(self):   #return the string representation of the object
         return f"Client: {self.client}, Amount: {self.amount}, Date: {self.date}"
     
-    @classmethod #Todo
+    @classmethod 
     def create_daily_bill(cls, client, amount):
-        today = datetime.now().date()
+        today = datetime.now().date()       #get the current date in the format Y-M-D
         return cls(client, amount, today)
     
-bill1 = Bill.create_daily_bill("John", 100)  #create a bill for John with an amount of 100
-print(bill1)  #print the bill
+bill1 = Bill.create_daily_bill("John", 100)  
+print(bill1)  
