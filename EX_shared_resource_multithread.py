9b39c323b6946bc52ea20cf26b65f8aff01b1899
meglioLeo <leonardo.cresti@students.sarrocchi.it>
2024-11-09 12:46:20 +0100

    done exercise 3

--- EX_shared_resource_multithread.py
+++ EX_shared_resource_multithread.py
@@ -1,4 +1,21 @@
 import threading
 
-shared_counter = 0
+class Counter:
+    def __init__(self):
+        self.value = 1
+        self.lock = threading.Lock()
+        
+    def increment(self, thread_namae):
+        with self.lock:
+            self.value = self.value * 1000
+            print(f"{thread_namae} incremented counter to: {self.value}")
+                
+shared_counter = Counter()
+                
+threads = [threading.Thread(target=shared_counter.increment, args=(f"thread{i+1}",)) for i in range(10)]
 
+for t in threads:
+    t.start()
+    
+for t in threads:
+    t.join()
