0e41b8fd7b08990882fb20699e41ef3d1f91e817
meglioLeo <leonardo.cresti@students.sarrocchi.it>
2024-11-09 12:01:58 +0100

    Done ex2

--- ex2/EX_block_multithread.py
+++ ex2/EX_block_multithread.py
@@ -3,10 +3,27 @@ import time
 
 class Block:
     def __init__(self):
-        self.value = 1
+        self.value = 0
+        self.lock = threading.Lock()
         
     def calculate(self):
         for i in range(1000000):
-            self.value += 1
-            
+            with self.lock:
+                self.value += 1
     
+    def print_status(self, thread_name):
+        while thread_name.is_alive():
+            with self.lock:
+                print(self.value)
+                time.sleep(0.5)
+            
+block = Block()
+
+thread1 = threading.Thread(target=block.calculate)
+thread2 = threading.Thread(target=block.print_status, args=(thread1,))
+
+thread1.start()
+thread2.start()
+
+thread1.join()
+thread2.join()  
