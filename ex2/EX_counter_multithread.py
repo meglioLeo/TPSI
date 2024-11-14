e3f4bc7c57f9022192b0fad5321117eaf7239023
meglioLeo <leonardo.cresti@students.sarrocchi.it>
2024-11-14 17:51:09 +0100

    added comments to counter_multithread

--- ex2/EX_counter_multithread.py
+++ ex2/EX_counter_multithread.py
@@ -3,33 +3,31 @@ import time
 
 class Counter:
     def __init__(self, start_value):
-        self.value = start_value
-        self.lock = threading.Lock()
+        self.value = start_value      # Initialize the value that will be changed by the threads
+        self.lock = threading.Lock()  # Create a lock object to synchronize access to the shared resource
 
     def increment(self):
         with self.lock:
             for i in range(10):
                 self.value += 1
-                print(self.value)
-                time.sleep(1)
+                print(f"Incrementi thread: {self.value}")
+                time.sleep(1)   # Simulate a delay to make the thread execution slower
 
     def decrement(self):
         with self.lock:
             for i in range(10):
-                self.value -= 1
-                print(self.value)
-                time.sleep(1)
+                self.value -= 1     
+                print(f"Decrementing thread: {self.value}")
+                time.sleep(1)   # Simulate a delay to make the thread execution slower
 
-counter1 = Counter(0)
-counter2 = Counter(11)
+counter1 = Counter(0)   
+counter2 = Counter(11)  
 
-thread1 = threading.Thread(target=counter1.increment)
+thread1 = threading.Thread(target=counter1.increment)     # Create a thread that will increment the counter
 thread2 = threading.Thread(target=counter2.decrement)
 
-thread1.start()
-
+thread1.start()   # Start the thread
 thread2.start()
 
-thread1.join()
-
+thread1.join()    # Wait for the thread to finish
 thread2.join()
