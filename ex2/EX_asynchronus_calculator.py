2257ea131afa9269893f24e74c32d2c26f9d7a8a
meglioLeo <leonardo.cresti@students.sarrocchi.it>
2024-11-14 18:08:30 +0100

    added comments to asynchronus_calculator

--- ex2/EX_asynchronus_calculator.py
+++ ex2/EX_asynchronus_calculator.py
@@ -1,6 +1,6 @@
 import asyncio
 
-async def factorial(n):
+async def factorial(n):  #This function is a coroutine that calculates the factorial of a number
     result = 1
     for i in range(1, n+1):
         result *= i
@@ -8,10 +8,10 @@ async def factorial(n):
     print(f"Calculated factorial of {n} and the result is {result}")
     return result
 
-async def main():
-    numbers = [3, 4, 5, 6, 7, 8, 9, 10]
-    tasks = [factorial(n) for n in numbers]
-    results = await asyncio.gather(*tasks)
+async def main():   #This function is a coroutine that creates a list of tasks and waits for them to finish
+    numbers = [3, 4, 5, 6, 7, 8, 9, 10]   #List of numbers to calculate the factorial
+    tasks = [factorial(n) for n in numbers]   #Create a list of tasks
+    results = await asyncio.gather(*tasks)    #Wait for all tasks to finish and get the results
     print(f"Results: {results}")
     
-asyncio.run(main())
+asyncio.run(main())    #Run the main coroutine
