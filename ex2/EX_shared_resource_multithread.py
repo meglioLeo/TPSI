e1cad0610feb8940b9997b56e8c6c89b97525762
meglioLeo <leonardo.cresti@students.sarrocchi.it>
2024-11-14 18:06:43 +0100

    added comments to shared_resourc_multithread

--- ex2/EX_shared_resource_multithread.py
+++ ex2/EX_shared_resource_multithread.py
@@ -1,4 +1,5 @@
 import threading
+import time
 
 class Counter:
     def __init__(self):
@@ -7,12 +8,14 @@ class Counter:
         
     def increment(self, thread_namae):
         with self.lock:
-            self.value = self.value * 1000
+            self.value = self.value * 1000   #Simulate a long running calculation
             print(f"{thread_namae} incremented counter to: {self.value}")
+            time.sleep(0.5)   #Simulate a delay to show the effect of multithreading
                 
 shared_counter = Counter()
-                
-threads = [threading.Thread(target=shared_counter.increment, args=(f"thread{i+1}",)) for i in range(10)]
+
+#Create a list of 10 threads                
+threads = [threading.Thread(target=shared_counter.increment, args=(f"thread {i+1}",)) for i in range(10)]  
 
 for t in threads:
     t.start()
