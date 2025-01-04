1023a8278eb4b15d68dabc24caa7c9e3a99a354f
meglioLeo <leonardo.cresti@students.sarrocchi.it>
2025-01-04 11:33:38 +0100

    created class operation_in_xml with its methods. Storage inventroy completed

--- ex4/EX_storage_inventory/main.py
+++ ex4/EX_storage_inventory/main.py
@@ -2,6 +2,8 @@ import pprint #module for pretty printing
 import xmlschema #module for validating xml files
 import logging #module for logging
 import os #module for interacting with the operating system
+from datetime import datetime #module for working with dates
+import xml.etree.ElementTree as ET #module for parsing xml files
 
 #get the path of the script
 global script_path
@@ -61,6 +63,35 @@ def calculate_stock(data):
                     elif operation["operation_type"] == "out":
                         stock -= int(operation["operation_quantity"])
             print(f"Product {product_name} with code {product_code} has {stock} units in stock")
+            
+class operation_in_xml:
+    def __init__(self):
+        self.operations = []
+        
+    def get_operations(self, data, start_date, end_date):
+        for operation in data["operation_register"]["operation"]:
+            operation_date = operation["operation_date"]
+            operation_date = datetime.fromisoformat(operation_date)
+            if start_date <= operation_date <= end_date:
+                operation_details = {
+                    "operation_date": operation["operation_date"],
+                    "operation_time": operation["operation_time"],
+                    "product_involved": operation["operation_product"],
+                    "quantity": operation["operation_quantity"],
+                    "operation_type": operation["operation_type"]
+                }
+                self.operations.append(operation_details)
+        #pprint.pprint(self.operations, indent=2, width=100)
+    
+    def to_xml(self, xml_destination):
+        root = ET.Element("operation_resume")
+        for operation in self.operations:
+            operation_element = ET.SubElement(root, "operation")
+            for key, value in operation.items():
+                ET.SubElement(operation_element, key).text = str(value)
+        tree = ET.ElementTree(root)
+        with open(xml_destination, "wb") as f:
+            tree.write(f, encoding="utf-8", xml_declaration=True)               
                         
 def main():
     xml_file = os.path.join(script_path, "storage_inventory.xml")
@@ -69,8 +100,11 @@ def main():
     data = get_data_from_xml(xml_file,xsd_file)
     if data is None:
         return
-    calculate_total_value(data)
-    calculate_stock(data)
+    #calculate_total_value(data)
+    #calculate_stock(data)
+    resume = operation_in_xml()
+    resume.get_operations(data, datetime(2025, 1, 1), datetime(2025, 1, 2))
+    resume.to_xml(os.path.join(script_path, "operation_resume.xml"))
     
 if __name__ == "__main__":
     main()
