0def10fe57117be0e02e7fdd6251479e90979094
meglioLeo <leonardo.cresti@students.sarrocchi.it>
2025-02-01 23:43:45 +0100

    Added the calculate outcome and update json file functions to main.py in medical results

--- ex_json/EX_medical_result/main.py
+++ ex_json/EX_medical_result/main.py
@@ -2,6 +2,44 @@ import json
 import os
 from jsonschema import validate, ValidationError
 
+class MedicalResult:
+    def __init__(self, patientName, patientSurname, samplingDate, analysisType, result, unitOfMeasure, resultRange, outcome):
+        self.patientName = patientName
+        self.patientSurname = patientSurname
+        self.samplingDate = samplingDate
+        self.analysisType = analysisType
+        self.result = result
+        self.unitOfMeasure = unitOfMeasure
+        self.resultRange = resultRange
+        self.outcome = outcome
+        
+    def to_dict(self):
+        return {
+            "patientName": self.patientName,
+            "patientSurname": self.patientSurname,
+            "samplingDate": self.samplingDate,
+            "analysisType": self.analysisType,
+            "result": self.result,
+            "unitOfMeasure": self.unitOfMeasure,
+            "resultRange": self.resultRange,
+            "outcome": self.outcome
+        }
+        
+    @classmethod
+    def from_dict(cls, data):
+        return cls(
+            patientName=data["patientName"],
+            patientSurname=data["patientSurname"],
+            samplingDate=data["samplingDate"],
+            analysisType=data["analysisType"],
+            result=data["result"],
+            unitOfMeasure=data["unitOfMeasure"],
+            resultRange=data["resultRange"],
+            outcome=data["outcome"]
+        )
+        
+    def update_outcome(self, outcome):
+        self.outcome = outcome
 
 def validate_result (json_file_path, schema_file_path):
     with open(schema_file_path, "r") as schema_file:
@@ -25,6 +63,52 @@ def validate_result (json_file_path, schema_file_path):
     else:
         print("The medical result data is valid.")
         
+def calculate_outcome(json_file_path):
+    
+    with open(json_file_path, "r") as json_file:
+        data = json.load(json_file)
+        
+    medical_results = []
+    
+    for exams in data["medicalExams"]:
+        for exam in exams:
+            medical_result = MedicalResult.from_dict(exam)
+            medical_results.append(medical_result)
+            
+    for exam in medical_results:
+        result_range = exam.resultRange
+        min_value = result_range["min"]
+        max_value = result_range["max"]
+        result = exam.result
+        if result < min_value:
+            outcome = "Low"
+        elif result > max_value:
+            outcome = "High"
+        else:
+            outcome = "Normal"
+        exam.update_outcome(outcome)
+    
+    # print("Medical results with outcomes:")
+    # for idx, exam in enumerate(medical_results):
+    #     print(f"Exam {idx + 1}: {exam.to_dict()}")
+    
+    update_json_file(json_file_path, medical_results)
+    
+def update_json_file(json_file_path, medical_results):
+    
+    updated_exams_group = [exam.to_dict() for exam in medical_results]
+    updated_data = {
+        "medicalExams": [
+            updated_exams_group
+        ]
+    }
+    
+    with open(json_file_path, "w") as json_file:
+        json.dump(updated_data, json_file, indent=4)
+    print(f"Il file {json_file_path} è stato aggiornato.")
+        
+        
+        
 if __name__ == "__main__":
     script_dir = os.path.dirname(os.path.abspath(__file__))
     
@@ -32,3 +116,4 @@ if __name__ == "__main__":
     schema_file_path = os.path.join(script_dir, "medical_result.schema.json")
     
     validate_result(json_file_path, schema_file_path)
+    calculate_outcome(json_file_path)
