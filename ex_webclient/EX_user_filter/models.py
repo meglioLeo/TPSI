ec04fdbdf305eb210028d6b71b4266606c418264
meglioLeo <leonardo.cresti@students.sarrocchi.it>
2025-03-12 10:50:25 +0100

    Added models in user filter

---
+++ ex_webclient/EX_user_filter/models.py
@@ -0,0 +1,15 @@
+class UserModel:
+    def __init__(self, id, name, company, username, email, address, zip, state, country, phone, photo):
+        self.id = id
+        self.name = name
+        self.company = company
+        self.username = username
+        self.email = email
+        self.address = address
+        self.zip = zip
+        self.state = state
+        self.country = country
+        self.phone = phone
+        self.photo = photo
+
+    
