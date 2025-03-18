8a63f28ca0efe5782a597d7778954dc5c14f927a
meglioLeo <leonardo.cresti@students.sarrocchi.it>
2025-03-18 21:53:36 +0100

    Add configuration file with the api url and the default filter

--- ex_webclient/EX_companies_filter/filter.py
+++ ex_webclient/EX_companies_filter/filter.py
@@ -2,7 +2,7 @@ from config import DEFAULT_EMPLOYEE_COUNT_FILTER
 
 class CompanyFilter:
     @staticmethod
-    def filter_by_employee_count(companies, state=DEFAULT_EMPLOYEE_COUNT_FILTER):
+    def filter_by_employee_count(companies, employee_count=DEFAULT_EMPLOYEE_COUNT_FILTER):
         filtered_companies = []
         for company in companies:
             if company.employeeCount > DEFAULT_EMPLOYEE_COUNT_FILTER:
