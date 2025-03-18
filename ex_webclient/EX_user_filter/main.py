bc6b8e856d33d328b63c81faab539d3a6cbe9042
meglioLeo <leonardo.cresti@students.sarrocchi.it>
2025-03-18 17:36:59 +0100

    Solved a bug with a return in main.py, by moving it sideways by one tab

--- ex_webclient/EX_user_filter/main.py
+++ ex_webclient/EX_user_filter/main.py
@@ -7,7 +7,7 @@ def main():
     users = fetcher.fetch_users() # Fetch user list as UserModel instances
     if not users:
         print("No users found or API request failed.")
-    return
+        return
     filtered_users = UserFilter.filter_by_state(users) # Filter users by state
     print("\nUsers in Illinois:")
     for user in filtered_users:
