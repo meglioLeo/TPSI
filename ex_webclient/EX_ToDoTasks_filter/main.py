39c0d160dd4bad07ddb5416148b203b18ca51074
meglioLeo <leonardo.cresti@students.sarrocchi.it>
2025-03-18 22:19:03 +0100

    Implement main script to fetch and filter tasks

--- ex_webclient/EX_ToDoTasks_filter/main.py
+++ ex_webclient/EX_ToDoTasks_filter/main.py
@@ -1,23 +1,16 @@
-import requests
-import json
-import pprint
+from fetcher import TaskFetcher
+from filter import TaskFilter
 
-url = "https://dummy-json.mock.beeceptor.com/todos"
-headers = {"Accept": "application/json"}
-
-response = requests.get(url, headers=headers)
-
-if response.status_code == 200:
-    
-    list_of_tasks = json.loads(response.text)
-    
-    filtered_list = []
-    
-    for task in list_of_tasks:
-        if task["completed"] == False:
-            filtered_list.append(task)
-            
-    pprint.pprint(filtered_list)
-    
-else:
-    print("Error: ", response.status_code)
+def main():
+    fetcher = TaskFetcher()
+    tasks = fetcher.fetch_tasks()
+    if not tasks:
+        print("No tasks found or API request failed.")
+        return
+    filtered_tasks = TaskFilter.filter_by_completed(tasks)
+    print("Uncompleted tasks:")
+    for task in filtered_tasks:
+        print(f"- {task.title}")
+        
+if __name__ == "__main__":
+    main()
