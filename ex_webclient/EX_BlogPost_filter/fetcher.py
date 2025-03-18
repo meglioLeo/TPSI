e9134dcde03bd8b16bb3bd5fcad4b52784ac62c1
meglioLeo <leonardo.cresti@students.sarrocchi.it>
2025-03-18 22:43:29 +0100

    Modified fetcher, added the class and the init method

--- ex_webclient/EX_BlogPost_filter/fetcher.py
+++ ex_webclient/EX_BlogPost_filter/fetcher.py
@@ -2,12 +2,16 @@ import requests
 from config import API_URL
 from models import PostModel
 
-def fetch_posts():
-    try:
-        response = requests.get(API_URL)
-        response.raise_for_status()
-        posts_json = response.json()
-        return [PostModel.from_json(post) for post in posts_json]
-    except requests.exceptions.RequestException as e:
-        print(f"HTTP request error: {e}")
-        return []
+class PostFetcher:
+    def __init__(self, url=API_URL):
+        self.url = url
+
+    def fetch_posts():
+        try:
+            response = requests.get(API_URL)
+            response.raise_for_status()
+            posts_json = response.json()
+            return [PostModel.from_json(post) for post in posts_json]
+        except requests.exceptions.RequestException as e:
+            print(f"HTTP request error: {e}")
+            return []
--- ex_webclient/EX_BlogPost_filter/main.py
+++ ex_webclient/EX_BlogPost_filter/main.py
@@ -1,23 +1,5 @@
-import requests
-import json
-import pprint
+from fetcher import fetch_posts
+from filter import PostFilter
 
-url = "https://dummy-json.mock.beeceptor.com/posts"
-headers = {"Accept":"application/json"}
-
-response = requests.get(url, headers = headers)
-
-if response.status_code == 200:
-    
-    posts = json.loads(response.text)
-   
-    filtered_posts = []
-    
-    for post in posts:
-        if post["comment_count"] > 5:
-            filtered_posts.append(post)
-            
-    pprint.pprint(filtered_posts)
-     
-else:
-    print("Error: ", response.status_code) 
+def main():
+    fetcher = 
