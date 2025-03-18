819daf2cdfa52be71b16f2ee204e7cfae5e04fb5
meglioLeo <leonardo.cresti@students.sarrocchi.it>
2025-03-18 17:20:06 +0100

    modified models according to the teacher code

--- ex_webclient/EX_user_filter/models.py
+++ ex_webclient/EX_user_filter/models.py
@@ -1,12 +1,13 @@
 class UserModel:
-    def __init__(self, id, name, company, username, email, address, zip, state, country, phone, photo):
+
+    def __init__(self, id, name, company, username, email, address, zip_code, state, country, phone, photo):
         self.id = id
         self.name = name
         self.company = company
         self.username = username
         self.email = email
         self.address = address
-        self.zip = zip
+        self.zip_code = zip_code
         self.state = state
         self.country = country
         self.phone = phone
@@ -15,18 +16,18 @@ class UserModel:
     @classmethod
     def from_json(cls, data):
         return cls(
-            id = data.get("id"),
-            name = data.get("name"),
-            company = data.get("company"),
-            username = data.get("username"),
-            email = data.get("email"),
-            address = data.get("address"),
-            zip = data.get("zip"),
-            state = data.get("state"),
-            country = data.get("country"),
-            phone = data.get("phone"),
-            photo = data.get("photo")
+            id=data.get("id"),
+            name=data.get("name"),
+            company=data.get("company"),
+            username=data.get("username"),
+            email=data.get("email"),
+            address=data.get("address"),
+            zip_code=data.get("zip"), 
+            state=data.get("state"),
+            country=data.get("country"),
+            phone=data.get("phone"),
+            photo=data.get("photo")
         )
-        
-    def __repr__ (self):
-        return f"UserModel(id = {self.id}, name = {self.name}, state = {self.state})"
+
+    def __repr__(self):
+        return f"UserModel(id={self.id}, name='{self.name}', state='{self.state}')"
