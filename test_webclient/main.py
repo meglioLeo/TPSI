2a1b3d6fd6597114a51e0e11dd6c0415fad1071a
meglioLeo <leonardo.cresti@students.sarrocchi.it>
2025-03-19 09:53:41 +0100

    Implemented total_time in main

--- test_webclient/main.py
+++ test_webclient/main.py
@@ -1,8 +1,7 @@
 from fetcher import TransactionFetcher
 from filter import count_enter_Livorno
 from filter import amount_spent_in_year
-#from filter import total_time
-from datetime import datetime
+from filter import total_time
 
 def main():
     fetcher = TransactionFetcher()
@@ -15,7 +14,10 @@ def main():
     print(f"User entered from Livorno "{n_times_enter_livorno}" times")
 
     amount_spent = amount_spent_in_year(transactions)
-    print(f"User spent in total "{amount_spent}" euro")
+    print(f"User spent in total "{amount_spent}" euros")
+
+    time_spent = total_time(transactions)
+    print(f"User spent in total "{time_spent}" hours")
 
 if __name__=="__main__":
     main()
