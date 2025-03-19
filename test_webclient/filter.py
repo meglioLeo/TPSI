4eafa2faab2296c0441ecbac7af45fa5402242d7
meglioLeo <leonardo.cresti@students.sarrocchi.it>
2025-03-19 09:41:12 +0100

    Implemented amount_spent_in_year to calculate the total import in one year

--- test_webclient/filter.py
+++ test_webclient/filter.py
@@ -3,3 +3,5 @@ from datetime import datetime
 def count_enter_Livorno(transactions):
     return sum(1 for transaction in transactions if transaction.enter_place == "Livrono")
 
+def amount_spent_in_year(transactions):
+    return sum(transaction.amount for transaction in transactions)
