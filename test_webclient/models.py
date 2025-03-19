9243610a00646801e7a369af9ee380be7cbfdd54
meglioLeo <leonardo.cresti@students.sarrocchi.it>
2025-03-19 08:49:24 +0100

    Add the models file for structured payment models

---
+++ test_webclient/models.py
@@ -0,0 +1,23 @@
+class PaymentModel:
+
+    def __init__(self, code, enter_place, enter_hour, exit_place, exit_hour, amount):
+        self.code = code,
+        self.enter_place = enter_place
+        self.enter_hour = enter_hour
+        self.exit_place = exit_place
+        self.exit_hour = exit_hour
+        self.amount = amount
+    
+    @classmethod
+    def from_json(cls, data):
+        return(
+            code = data.get("codice"),
+            enter_place = data.get("ingresso"),
+            enter_hour = data.get("ora_ingresso"),
+            exit_place = data.get("uscita"),
+            exit_hour = data.get("ora_uscita"),
+            amount = data.get("importo")
+        )
+
+    def __repr__(self):
+        return f"PaymentModel(code={self.code}, enter_place='{self.enter_place}', enter_hour='{self.enter_hour}', exit_hour='{self.exit_hour}', amount={self.amount})"
