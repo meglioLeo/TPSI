d90097b994c56efcfe233b47bdb10a15fe2f9e9f
meglioLeo <leonardo.cresti@students.sarrocchi.it>
2025-03-19 09:48:35 +0100

    Implemented main to fetch data and do calculations

---
+++ test_webclient/main.py
@@ -0,0 +1,21 @@
+from fetcher import TransactionFetcher
+from filter import count_enter_Livorno
+from filter import amount_spent_in_year
+#from filter import total_hour
+from datetime import datetime
+
+def main():
+    fetcher = TransactionFetcher()
+    transactions = fetcher.fetch_transactions
+    if not transactions:
+        print("No users found or URL request failed.")
+        return
+    
+    n_times_enter_livorno = count_enter_Livorno(transactions)
+    print(f"User entered from Livorno "{n_times_enter_livorno}" times")
+
+    amount_spent = amount_spent_in_year(transactions)
+    print(f"User spent in total "{amount_spent}" euro")
+
+if __name__=="__main__":
+    main()
