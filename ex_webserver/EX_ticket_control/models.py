3fc8b0c1c798a21f8138ed5cda1dec5d25aa54b0
meglioLeo <leonardo.cresti@students.sarrocchi.it>
2025-04-23 23:19:58 +0200

    Fixed mispelling error

--- ex_webserver/EX_ticket_control/models.py
+++ ex_webserver/EX_ticket_control/models.py
@@ -5,7 +5,7 @@ db = SQLAlchemy()
 
 class Ticket(db.Model):
     __tablename__ = "tickets"
-    code = db.Column(db.String(12), priary_key=True)
+    code = db.Column(db.String(12), primary_key=True)
     valid = db.Column(db.String(1), nullable=False, default="Y")
     
     def __repr__(self):
