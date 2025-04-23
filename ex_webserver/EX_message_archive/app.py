ebdc29b9227f6c7ff473c3d7e16a20c2c9418507
meglioLeo <leonardo.cresti@students.sarrocchi.it>
2025-04-23 16:53:09 +0200

    Added in the dedicated branch helper functions to create xml responses

--- ex_webserver/EX_message_archive/app.py
+++ ex_webserver/EX_message_archive/app.py
@@ -1,12 +1,30 @@
-from flask import jsonify, Flask, request
+from flask import jsonify, Flask, request, Response     #Response is needed for having control on the xml response
 from models import db, Message
-from datetime import datetime
+#from datetime import datetime
 import re
 from config import Config
+import xml.etree.ElementTree as ET
 
 app = Flask(__name__)
 app.config.from_object(Config)
 db.init_app(app)
+
+#helper function to create the xml code for the response
+def message_to_xml(message):
+    msg_elem = ET.Element("message")
+    ts_elem = ET.SubElement(msg_elem, "timestamp")
+    ts_elem.text = message.timestamp
+    text_elem = ET.SubElement(msg_elem, "text")
+    text_elem.text = message.text
+    return msg_elem
+
+#helper function if there are multiple messages
+def messages_to_xml(messages):
+    root = ET.Element("message-list")
+    for message in messages:
+        msg_elem = message_to_xml(message)
+        root.append(msg_elem)
+    return root
 
 @app.route('/Messages', methods=['GET'])
 def get_messages():
