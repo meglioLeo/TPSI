7e3e81555ccd7833d780cac632843efbca1e57b1
meglioLeo <leonardo.cresti@students.sarrocchi.it>
2025-04-23 22:32:23 +0200

    Added models.py with the model representing how data will be saved in the db

---
+++ ex_webserver/EX_ticket_control/models.py
@@ -0,0 +1,12 @@
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+db = SQLAlchemy()
+
+class Ticket(db.Model):
+    __tablename__ = "tickets"
+    code = db.Column(db.String(12), priary_key=True)
+    valid = db.Column(db.String(1), nullable=False, default="Y")
+    
+    def __repr__(self):
+        return f"<Ticket {self.code} valid: {self.valid}>"
