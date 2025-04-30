07c9c6a7e813d5bb06f9387e7eedf2df0e27a744
meglioLeo <leonardo.cresti@students.sarrocchi.it>
2025-04-30 08:45:15 +0200

    Defined city model in models.py

---
+++ test_webserver/models.py
@@ -0,0 +1,11 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class City(db.Model):
+    __tablename__ = "cities"
+    ID = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(100), nullable = False)
+    country = db.Column(db.String(100), nullable = False)
+    area = db.Column(db.Float, nullable = False)
+    last_detection = db.Column(db.String, nullable = False)   # YYYY-MM-DD format
