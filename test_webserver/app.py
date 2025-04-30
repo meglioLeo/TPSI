7de26c806dd2380525ba2e75698cd205581079cd
meglioLeo <leonardo.cresti@students.sarrocchi.it>
2025-04-30 08:59:21 +0200

    Added the first CRUD endpoint to get all cities

--- test_webserver/app.py
+++ test_webserver/app.py
@@ -6,3 +6,23 @@ app = Flask(__name__)
 app.config.from_object(Config)
 db.init_app(app)
 
+# Get all cities endpoint
+@app.route('/cities', methods=['GET'])
+def get_cities():
+    cities = City.query.all()
+
+    if not cities:
+        return [], 200   #no city registered, no need to return a 404
+    
+    result = []
+
+    for city in cities:
+        result.append({
+            "ID": city.ID,
+            "name": city.name,
+            "country": city.country,
+            "area": city.area,
+            "last_detection": city.last_detection
+        })
+
+    return jsonify(result), 200
